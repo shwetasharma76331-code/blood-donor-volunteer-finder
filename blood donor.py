e92a5c963125ca45978aee31e9434bf31cd38b12
unknown <shwetasharma76331@gmail.com>
2026-09-16 20:34:51 +0530

    complete day 2 interface

--- blood donor.py
+++ blood donor.py
@@ -1,82 +1,125 @@
 import streamlit as st
-with open("style.css") as f:
-    st.markdown(
-        f"<style>{f.read()}</style>",
-        unsafe_allow_html=true
-    )
-
 st.set_page_config(
     page_title="BloodConnect",
     layout="wide"
 )
-st.title("BloodConnect")
-st.subheader("Blood Donor & Volunteer Finder")
+# CSS
+st.markdown("""
+<style>
+.main-title{
+     font-size: 42px;
+     font-weight: bold;
+     text-align: center;
+     margin-bottom: 10px;
+}
+.subtitle {
+    font-size: 22px;
+    text-align: center;
+    margin-bottom: 25px;
+}
+.card{
+    padding: 25px;
+    border-radius: 12px;
+    text-align: center;
+    margin: 10px;
+}
+.second-title{
+    font-size: 28px;
+    font-weight: bold;
+    margin-top: 25p;
+}
+</style>
+""", unsafe_allow_html=True)
+#HTML
+st.markdown(
+    '<divclass="main-title">BloodConnect</div>',
+    unsafe_allow_html=True
+) 
+st.markdown(
+    '<div class="subtitle">Blood Donor & Volunteer Finder</div>',
+    unsafe_allow_html=True
+)  
 st.write(
-    "Connecting people who need blood with donors volunteers."
+    "A platform that helps connect people who need blood"
+    "with suitable donors and volunteers."
 )
 st.divider()
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.header("Find a Donor")
-    st.write("Search for a suitable blood donor.")
-    st.button("Find Donor")
-with col2:
-    st.header("Register as Donor")
-    st.write("Register yourself as a blood donor.")
-    st.button("Register Donor")
-with col3:
-    st.header("Become a Volunteer")
-    st.write("Join us and help in people in need.")
-    st.button("Register Volunteer")
-st.divider()
-st.header("How BloodConnect Works")
-st.write("""
-1. Search for the required blood group.
-2. Find available donors.
-3. Donors can register their dettails.
-4. Volunteers can register to help.""")            
-
-import streamlit as st
-st.set_page_config(
-    page_title="BloodConnect",
-    layout="wide"
-)
-st.markdown(
-    '<div class="main-title">BloodConnect</div>',
-    unsafe_allow_html=true
-)
-st.markdown(
-    '<div class="main-title">Blood Donor & Volunteer Finder</div>',
-    unsafe_allow_html=true
-)
-st.subheader("Blood Donor & Volunteer Finer")
-st.write("A platform that helps  connect people who need blood" \
-"with suitable donors and volunteers.")
-st.divider()
-menu = st.sidebar.selectbox(
-    "select page",
-    ["Home","Find Donor",
-     "Register as Donor","Volunteer","About"]
-)
+menu=st.sidebar.selectbox(
+    "Select Page",
+    ["Home", "Find Donor",
+     "Register as Donor", "Volunteer",
+     "About"]
+)        
 if menu == "Home":
-    st.header("Welcome to Blood Connect")
-    st.write("BloodConnect makes it easier to find blood donors" \
-    "and volunteers based on blood group and location. ")
-    col1 ,col2,col3 = st.columns(3)
+    st.header("Welcome to BloodConnect")
+    st.write(
+        "BloodConnect makes it easier to find blood donors"
+        "and volunteers based on blood group and location."
+         )
+    col1, col2, col3 = st.columns(3)
     with col1:
-        st.subheader("Find a Donor")
-st.write("Search for donors according to blood group and location.")
-with col2:
-    st.subheader("Register as a donor")
-    st.write("Register your details to help people in need.")
+        st.markdown(
+            '<div class="card"><h3>Find a Donor</h3>'
+            '<p>Search donors according to blood group and location.</p></div>',
+            unsafe_allow_html=True
+        )
+    with col2:
+        st.markdown(
+            '<div class="card"><h3>Register as a Donor</h3>'
+            '<p>Register your details to help people in need.</p></div',
+            unsafe_allow_html=True
+        )
     with col3:
-        st.subheader("Become a Volunteer")
-st.write("Join as a volunteer and support the community.")
-st.divider()
-st.header("How BloodConnect Works")
-st.write("""
-1. Select the required blood group.
-2. Enter your location.
-3. Search for available donors.
-4. Donors and volunteers can register themselves.
-""")
+        st.markdown(
+            '<div class="card"><h3>Become a Volunteer</h3>'
+            '<p>Join as a volunteer and support the community.</p><div>',
+            unsafe_allow_html=True
+        )    
+        st.divider()
+        st.header("How BloodConnect Works")
+        st.write("""
+        1. Select the required blood group.
+        2. Enter your location.
+        3. Search for available donors.
+        4. Donors and Volunteers can register themselves.
+          """)
+elif menu == "Find Donor":
+    st.header("Find a Blood Donor")
+    st.info("Donor search feature will be added in Day 5.")
+elif menu == "Register as Donor":
+    st.header("Donor registeration form will be added in Day 3.")
+elif menu == "Volunteer":
+    st.header("About BloodConnect")
+    st.write(
+        "BloodConnect is a college developed  using"
+        "Pyhton,Streamlit,HTML,and CSS."
+    )                                                                                                                                                          
+elif menu == "Register as Donor":
+    st.header("Donor Registration")
+    st.write("Register yourself as a blood donor.")
+    name = st.text_input("Full Name")
+    age = st.number_input(
+        "Age",
+        min_value=18,
+        max_value=65,
+        step=1
+    )
+    blood_group = st.selectbox(
+        "Blood Group",
+        ["Select Blood Group",
+         "A+","A-","B+","B-",
+         "AB+","AB-","0+","0-"]
+    )
+    city = st.text_input("City/Location")
+    phone = st.text_input("Phone Number")
+if st.button("Register as Donor"):
+    if name == "":
+      st.error("Please enter your name.")
+    elif blood_group == "Select Blood Group":
+        st.error("Please enter your blood group.")
+    elif city == "":
+        st.error("Please enter your city.")
+    elif phone == "":
+        st.error("Please enter your phone number.")
+    else:
+        st.success("Donor registration submitted succesfully.")                     
